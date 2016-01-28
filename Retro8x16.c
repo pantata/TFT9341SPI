50c7610f0b85c695ad10903b38faa62b0da40ed3
Ludek Slouf <ludek@Ludeks-Mac-mini.local>
2016-01-28 16:42:46 +0100

    Added Arduino M0 compatibility

---
+++ Retro8x16.c
@@ -0,0 +1,9 @@
+//
+//  Retro8x16.c
+//  RLC3
+//
+//  Created by Ludek Slouf on 28.03.15.
+//  Copyright (c) 2015 Ludek Slouf. All rights reserved.
+//
+
+#include "Retro8x16.h"
---
+++ Retro8x16.h
@@ -0,0 +1,14 @@
+//
+//  Retro8x16.h
+//  RLC3
+//
+//  Created by Ludek Slouf on 28.03.15.
+//  Copyright (c) 2015 Ludek Slouf. All rights reserved.
+//
+
+#ifndef __RLC3__Retro8x16__
+#define __RLC3__Retro8x16__
+
+#include <stdio.h>
+
+#endif /* defined(__RLC3__Retro8x16__) */
